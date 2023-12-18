1a9e53693cf56cdcc86d8579d83a5a4fa0307043
Jordan Darlington <jordan.darlington@ii.co.uk>
2023-12-18 08:17:33 +0000

    adds python app to test repo

---
+++ app/test_main.py
@@ -0,0 +1,11 @@
+from main import home, reverse_string
+import unittest
+
+class TestMain(unittest.TestCase):
+    def test_home(self):
+        response = self.app.get('/')
+        self.assertEqual(response.data, b'Hello, World!')
+
+    def test_reverse_string(self):
+        response = self.app.get('/ReverseMe')
+        self.assertEqual(response.data, b'eMesreveR')
